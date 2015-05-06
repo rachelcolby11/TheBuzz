957ea005853c74d2971c3debd28391b4228f9d63
Rachel Colby <rachelcolby11@gmail.com>
2015-05-06 11:08:26 -0700

    Create wikis controller, routes, index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'welcome/index'
+  resources :wikis
 
-  get 'welcome/about'
+  devise_for :users
+  
+  get 'about' => 'welcome#about'
    
   root to: 'welcome#index'
 

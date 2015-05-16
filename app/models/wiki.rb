11eca9c0b05fe305e6754cf0bfc69836aba8c4cf
Rachel Colby <rachelcolby11@gmail.com>
2015-05-15 17:33:20 -0700

    Markdown v2 WIP

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -9,5 +9,22 @@ class Wiki < ActiveRecord::Base
     collaborators.where(user: user).first
   end
 
+  def markdown_title(title)
+    render_as_markdown @wiki.title
+  end
+
+  def markdown_body(body)
+    render_as_markdown @wiki.body
+  end
+
+  private
+
+  def render_as_markdown(attribute)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+  end
+
 end
 

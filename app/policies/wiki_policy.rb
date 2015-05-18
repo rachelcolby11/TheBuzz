fb7a321a2387a255b59dd2eded829ce752b47082
Rachel Colby <rachelcolby11@gmail.com>
2015-05-18 12:41:51 -0700

    Wiki collaborators permissions

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -3,39 +3,48 @@ class WikiPolicy < ApplicationPolicy
     true
   end
 
-   def show?
-    (user.present? && ((user.role == 'premium') || (user.role == 'admin'))) || record.private != true
+  def show?
+    record.private != true || (user.present? && ((user.role == 'admin') || record.user == user || record.users.include?(user)))  
+  end
+
+  def update?
+    show? 
+  end
+
+  def edit?
+    show? 
+  end
+
+  class Scope
+   attr_reader :user, :scope
+
+   def initialize(user, scope)
+     @user = user
+     @scope = scope
    end
 
-   class Scope
-    attr_reader :user, :scope
+   def resolve
+     wikis = []
+     if user.present? && user.role == 'admin'
+       wikis = scope.all 
+     elsif user.present? && user.role == 'premium'
+       all_wikis = scope.all
+       all_wikis.each do |wiki|
+         if wiki.private != true || wiki.user == user || wiki.users.include?(user)
+           wikis << wiki 
+         end
+       end
+     else 
+       all_wikis = scope.all
+       wikis = []
+       all_wikis.each do |wiki|
+         if wiki.private != true || wiki.users.include?(user)
+           wikis << wiki 
+         end
+       end
+     end
+     wikis  
+   end
+ end
 
-    def initializer(user, scope)
-      @user = user
-      @scope = scope
-    end
-
-    def resolve
-      wikis = []
-      if user.role == 'admin'
-        wikis = scope.all 
-      elsif user.role == 'premium'
-        all_wikis = scope.all
-        all_wikis.each do |wiki|
-          if wiki.private != true || wiki.user == user || wiki.users.include?(user)
-            wikis << wiki
-          end
-        end
-      else
-        all_wikis = scope.all
-        wikis = []
-        all_wikis.each do |wiki|
-          if wiki.private != true || wiki.users.include?(user)
-            wikis << wiki
-          end
-        end
-      end
-      wikis
-    end
-  end
 end
